8e2d1efc8886241d4a81cb56cabfd5cbf2cc1952
Ayilk <srangel.h92@gmail.com>
2022-08-15 13:25:22 -0500

    Marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+interface MarcadorColor{
+  color: string;
+  marker: mapboxgl.Marker;
+}
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
-  styles: [
+  styles: [`
+  .mapa-container{
+    height: 100%;
+    width: 100%;
+  }
+
+  .list-group {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    z-index: 999;
+  }
+
+  li{
+    cursor: pointer;
+  }
+  `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [ -106.44490237257102, 23.239929574080456];
+
+  //Arreglo de marcadores
+  marcadores: MarcadorColor[] = [];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel
+    });
+
+    // const markerHtml: HTMLElement = document.createElement('div');
+    // markerHtml.innerHTML = 'Hola mundo';
+
+    // const marker = new mapboxgl.Marker({element: markerHtml})
+    //   .setLngLat( this.center )
+    //   .addTo( this.mapa )
   }
+
+  agregarMarcador(){
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const nuevoMarcador = new mapboxgl.Marker({draggable: true, color})
+    .setLngLat( this.center )
+    .addTo( this.mapa)
+
+    this.marcadores.push({
+      color,
+      marker: nuevoMarcador
+    });
+
+    console.log(this.marcadores)
+   }
+
+   irMarcador(){
+    
+   }
 
 }
